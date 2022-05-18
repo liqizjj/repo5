9e58dd09b24e6d10b7a9e9bdc39ef0324e30a64d
liqi <liqi@itcast.cn>
2022-05-18 16:53:26 +0800

    第二次提交

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -9,7 +9,7 @@ public class FirstClass {
      //Hello test aaaaaaaaaaaaaaaaaaa
     }
     public void set(){
-        int o =1111111111;
+        int o =1111119111;
         int od =1111; int os =13188811;int ddos =666666666;
         int u=102;
         System.out.println(o);
