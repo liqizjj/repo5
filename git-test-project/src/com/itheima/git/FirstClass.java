9991c07caddb29c6171dbc0b443c748ef14f532b
liqi <liqi@itcast.cn>
2022-05-18 16:36:31 +0800

    第二次提交

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+
+     //Hello test aaaaaaaaaaaaaaaaaaa
+    }
+    public void set(){
+        int o =1111111111;
+        int od =1111; int os =13188811;int ddos =666666666;
+        int u=102;
+        System.out.println(o);
+    }
+}
